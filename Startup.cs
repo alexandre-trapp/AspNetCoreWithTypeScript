37c50f7cca60e1bb46adb28926ad004dcd9e58ac
Alexandre Cordeiro Trapp <alexandre.trapp@hotmail.com>
2020-06-16 21:50:29 -0300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreWithTypeScript
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) { }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
